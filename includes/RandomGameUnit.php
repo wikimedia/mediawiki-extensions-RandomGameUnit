69bcc1ae9f8246f59b626d72348e11bd2ddb2231
Jack Phoenix <ashley@uncyclomedia.co>
2020-10-26 06:10:33 +0200

    [SECURITY] Run stored, user-generated input from DB through htmlspecialchars() to avoid stored XSS originating from PictureGame/PollNY/QuizGame data
    Bug: T266400
    Change-Id: I497d2076038f75c9eb77e0e250f2af56f5bd2bfc

--- includes/RandomGameUnit.php
+++ includes/RandomGameUnit.php
@@ -168,10 +168,10 @@ class RandomGameUnit {
 			$ns = 300;
 		}
 
-		$poll_link = Title::makeTitle( $ns, $poll['title'] );
+		$poll_link = Title::makeTitleSafe( $ns, $poll['title'] );
 		$output = '<div class="game-unit-container">
 			<h2>' . wfMessage( 'game-unit-poll-title' )->escaped() . '</h2>
-			<div class="poll-unit-title">' . $poll_link->getText() . '</div>';
+			<div class="poll-unit-title">' . htmlspecialchars( $poll_link->getText(), ENT_QUOTES ) . '</div>';
 
 		if ( $poll['image'] ) {
 			$poll_image_width = $wgRandomImageSize;
@@ -193,7 +193,7 @@ class RandomGameUnit {
 		foreach ( $poll['choices'] as $choice ) {
 			$output .= '<a href="' . htmlspecialchars( $poll_link->getFullURL() ) . '" rel="nofollow">
 				<input id="poll_choice" type="radio" value="10" name="poll_choice" onclick="location.href=\'' .
-				htmlspecialchars( $poll_link->getFullURL() ) . '\'" /> ' . $choice['choice'] .
+				htmlspecialchars( $poll_link->getFullURL() ) . '\'" /> ' . htmlspecialchars( $choice['choice'], ENT_QUOTES ) .
 			'</a>';
 		}
 		$output .= '</div>
@@ -208,7 +208,7 @@ class RandomGameUnit {
 		$quiz_title = SpecialPage::getTitleFor( 'QuizGameHome' );
 		$output = '<div class="game-unit-container">
 			<h2>' . wfMessage( 'game-unit-quiz-title' )->escaped() . '</h2>
-			<div class="quiz-unit-title"><a href="' . htmlspecialchars( $quiz_title->getFullURL( "questionGameAction=renderPermalink&permalinkID={$quiz['id']}" ) ) . '" rel="nofollow">' . $quiz['text'] . '</a></div>';
+			<div class="quiz-unit-title"><a href="' . htmlspecialchars( $quiz_title->getFullURL( "questionGameAction=renderPermalink&permalinkID={$quiz['id']}" ) ) . '" rel="nofollow">' . htmlspecialchars( $quiz['text'], ENT_QUOTES ) . '</a></div>';
 
 		if ( $quiz['image'] ) {
 			$quiz_image_width = $wgRandomImageSize;
@@ -279,7 +279,7 @@ class RandomGameUnit {
 
 		$output = '<div class="game-unit-container">
 		<h2>' . wfMessage( 'game-unit-picturegame-title' )->escaped() . '</h2>
-		<div class="pg-unit-title">' . $title_text . '</div>
+		<div class="pg-unit-title">' . htmlspecialchars( $title_text, ENT_QUOTES ) . '</div>
 		<div class="pg-unit-pictures">
 			<div onmouseout="this.style.backgroundColor = \'\'" onmouseover="this.style.backgroundColor = \'#4B9AF6\'">
 				<a href="' . htmlspecialchars( $pic_game_link->getFullURL( 'picGameAction=renderPermalink&id=' . $picturegame['id'] . '&voteID=' . $picturegame['id'] . '&key=' . $key ) ) . '">' . $imgOne . '</a>
